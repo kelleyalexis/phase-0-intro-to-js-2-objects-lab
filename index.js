b19338c19fb682c956a602030b605d472b5900a3
Kelley Shackelford <kelleyalexis@yahoo.com>
2023-10-12 18:36:35 -0400

    defined functions for object

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+let employee = {
+    name: 'Abby',
+    streetAddress: 'Maplewood'
+};
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmployee = { ...employee }; updatedEmployee[key] = value;
+    return updatedEmployee;
+}
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+  function deleteFromEmployeeByKey(employee, key) {
+    const copyOfEmployee = { ...employee }; 
+    delete copyOfEmployee[key]; 
+    return copyOfEmployee; 
+}
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
